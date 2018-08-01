e8837c202afe533695c9c3011a779e97c0af3b5a
shivareddy <shiva.r547@gmail.com>
2018-08-01 08:54:14 +0530

    commit themes deleted

--- public/vinsol_spree_themes/theme-ScrollexTheme-3-3-bump/javascripts/default.manifest.js
+++
@@ -1,5 +0,0 @@
-// all javascript files related to themes should be require in this manifest.
-//= require modernizr.custom
-//= require bootstrap-number-input
-//= require navbar
-//= require custom
