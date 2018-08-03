fe61d049d2cb12a98e42a1a0ae469169a3d23e96
shivareddy <shiva.r547@gmail.com>
2018-08-03 22:27:36 +0530

    commit schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_25_153927) do
+ActiveRecord::Schema.define(version: 2018_08_03_162553) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
